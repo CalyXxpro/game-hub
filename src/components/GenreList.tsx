bbcdb0685ef4ecd6584b2461cab3476cfa999d1f
Aditya Lohumi <66105096+CalyXxpro@users.noreply.github.com>
2023-05-18 18:55:49 +0530

    comment unused code

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,10 @@
-import React from "react";
-import useData from "../hooks/useData";
 import useGenres from "../hooks/useGenres";
 import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
-import GenreListSkeleton from "./GenreListSkeleton";
+//import GenreListSkeleton from "./GenreListSkeleton";
 
 const GenreList = () => {
   const { data, error, isLoading } = useGenres();
-  const skeleton = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  //  const skeleton = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
   return (
     <>
       {error && null}
